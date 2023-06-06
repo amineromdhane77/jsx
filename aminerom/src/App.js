d8e640fb394b0c87b0d47afe5ddd0690a480c3be
amineromdhane77 <amineromdhane77@gmail.com>
2023-06-06 16:57:31 +0100

    first commit

---
+++ aminerom/src/App.js
@@ -0,0 +1,23 @@
+
+
+import Headerr from './composent/profile/Headerr';
+import Address from './composent/profile/Address';
+import ProfilePhoto from  './composent/profile/ProfilePhoto';
+import FullName from './composent/profile/FullName';
+
+function App() {
+  return (
+    <div >
+      <Headerr/> 
+      <ProfilePhoto/>
+      <FullName/>
+      <Address/>
+
+      
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ aminerom/src/composent/profile/ProfilePhoto.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Photo.css';
+function ProfilePhoto(){
+    return(
+        <div>
+            <h2>Card</h2>
+
+<div class="card">
+  <img src="https://www.simplilearn.com/ice9/free_resources_article_thumb/tester-or-developer-what-suits-you-the-most.jpg" alt="Avatar" style={{width:"100%"}}></img>
+
+  <div class="container">
+     
+    <p><strong style={{textAlign:"center"}}>Full stack JS</strong></p> 
+  </div>
+  </div>
+
+        </div>
+    )
+}
+export default ProfilePhoto;
